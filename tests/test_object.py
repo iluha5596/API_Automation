d1414fea30843fbd5386bce42e7837ba0866c437
iluha5596 <pantik96@mail.ru>
2024-02-20 16:59:05 +0300

    Проект по автоматизации API. Сделано 4 проверки

---
+++ tests/test_object.py
@@ -0,0 +1,42 @@
+from endpoints.create_object import CreateObject
+from endpoints.get_object import GetObject
+from endpoints.ubdate_object import UpdateObject
+from endpoints.delete_object import DeleteObject
+from data.read_data import ReadData
+
+
+class TestObject:
+
+    def test_create_object(self):
+        new_object_endpoint = CreateObject()
+        request_data_payload = ReadData('data/payload_create.json')
+        payload = request_data_payload.request_data()
+        new_object_endpoint.new_object(payload)
+        new_object_endpoint.check_response_is_200()
+        new_object_endpoint.check_name(payload['name'])
+
+    def test_get_object(self, obj_id):
+        get_object_endpoint = GetObject()
+        get_object_endpoint.get_by_id(obj_id)
+        get_object_endpoint.check_response_is_200()
+        get_object_endpoint.check_response_id(obj_id)
+
+    def test_update_object(self, obj_id):
+        update_object_endpoint = UpdateObject()
+        update_data_payload = ReadData('data/payload_update.json')
+        payload = update_data_payload.request_data()
+        update_object_endpoint.update_by_id(obj_id, payload)
+        update_object_endpoint.check_response_is_200()
+        update_object_endpoint.check_response_name(payload['name'])
+
+    def test_delete_object(self, obj_id):
+        delete_object_endpoint = DeleteObject()
+        delete_object_endpoint.delete_by_id(obj_id)
+        delete_object_endpoint.check_response_is_200()
+
+
+
+
+
+
+
