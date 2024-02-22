3cb6299285f5ab6bfd04cd464edd5aba63e7ee4a
iluha5596 <63452623+iluha5596@users.noreply.github.com>
2024-02-22 12:05:41 +0300

    Update base_endpoint.py

--- endpoints/base_endpoint.py
+++ endpoints/base_endpoint.py
@@ -8,3 +8,8 @@ class Endpoint:
     def check_response_is_200(self):
         with allure.step('Проверка, что код ответа == 200'):
             assert self.response.status_code == 200
+
+    def check_response_is_400(self):
+        with allure.step('Проверка, что код ответа == 400'):
+            assert self.response.status_code == 400, f'Статус код не равен 400, статус код == {self.response.status_code}'
+
